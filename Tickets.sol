ff9079b283eae3d2b95d325b28e2322fbce7c05f
Shubham Gadewar <gadewarshubham002@gmail.com>
2018-08-02 02:48:55 -0400

    Added the remaining functions

--- Tickets.sol
+++ Tickets.sol
@@ -37,5 +37,50 @@ contract Tickets{
     releaseEther = false;
   }
 
-  
+  function unlockEther (bytes32 hash) releaseTrue public notOwner{
+    uint amount = pendingTransaction[msg.sender];
+    pendingTransaction[msg.sender] = 0;
+    venueOwner.transfer(amount);
+    tickets[hash].paidFor = true;
+    emit paidFor(true);
+  }
+
+  function () public {
+    releaseEther = false;
+  }
+
+  function allowPurchase() public onlyOwner{
+    releseEther = True;
+    emit canPurchase(releseEther);
+  }
+
+  function createTicket() public payable onlyOwner{
+    if (msg.value == ticketPrice) {
+      pendingTransactions[msg.sender] = msg.value;
+      bytes32 hash = keccak256(abi.encodePacked(msg.sender));
+      tickets[hash] = Ticket(false, msg.sender);
+      emit TicketKey(hash);
+    }
+  }
+
+  function getTransaction() constant public returns (uint){
+    return pendingTransaction[msg.sender];
+  }
+
+  function getOwner(bytes32 hash) constant public returns (address){
+    return tickets[hash].owner;
+  }
+
+  function getTicketPrice() constant public return (uint){
+    return ticketPrice;
+  }
+
+  function getOwnerAdress() onlyOwner constant public returns (address){
+    return venueOwner;
+  }
+
+  function getEventName() constant public returns (string){
+    return name;
+  }
+
 }
