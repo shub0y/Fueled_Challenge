1e07c055020343657c1c41423c09973e977b5d09
Shubham Gadewar <gadewarshubham002@gmail.com>
2018-08-02 01:35:10 -0400

    Added modifiers

--- Tickets.sol
+++ Tickets.sol
@@ -7,4 +7,11 @@ contract Tickets{
   }
   mapping(bytes32 => Ticket) public tickets;
   mapping(address => uint) public pendingTransaction;
+  bool public releseEther;
+  uint public ticketPrice;
+  address public venueOwner;
+  string public name;
+  event TicketKey(bytes32 ticketKey);
+  event CanPurchase(bool canPurchase);
+  event PaidFor(bool paid);
 }
