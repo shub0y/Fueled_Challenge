b929ceb656285215d9ed8a7b8dfaef5ae5cb2e99
Shubham Gadewar <gadewarshubham002@gmail.com>
2018-08-02 00:57:18 -0400

    Created contracts and added mapping

--- Tickets.sol
+++ Tickets.sol
@@ -1,2 +1,10 @@
 pragma solidity ^0.4.21;
 
+contract Tickets{
+  struct Ticket{
+    bool paidFor;
+    address owner;
+  }
+  mapping(bytes32 => Ticket) public tickets;
+  mapping(address => uint) public pendingTransaction;
+}
