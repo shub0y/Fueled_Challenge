27a22e6c87547d8ccfc0fabd0846ba3e4ae4e1ee
Shubham Gadewar <gadewarshubham002@gmail.com>
2018-08-02 15:25:10 -0400

    Adding comments

--- Tickets.sol
+++ Tickets.sol
@@ -1,60 +1,71 @@
 pragma solidity ^0.4.21;
 
-contract Tickets{
-  struct Ticket{
+contract Tickets {
+  //Creating a struct for Ticket which will hold Owner address and whether paid or not
+  struct Ticket {
     bool paidFor;
     address owner;
   }
+
+  //Mapping for tickets to struct Tickets
   mapping(bytes32 => Ticket) public tickets;
-  mapping(address => uint) public pendingTransaction;
-  bool public releseEther;
-  uint public ticketPrice;
-  address public venueOwner;
-  string public name;
-  event TicketKey(bytes32 ticketKey);
-  event CanPurchase(bool canPurchase);
+  mapping(address => uint) public pendingTransactions;
+
+  bool public releaseEther; // releaseEther will be used to evaluate the state of the tx
+  uint public ticketPrice;  // Var for storing ticket Price
+  address public venueOwner; // Var for storing venueOwner
+  string public name; // Name of the venue
+  event TicketKey(bytes32 ticketKey); // Event to generate a ticket key
+  event CanPurchase(bool canPurchase); // A certain can purchase a ticket or not
   event PaidFor(bool paid);
 
-  modifier onlyOwner(){
+  // Only owner modifier for function
+  modifier onlyOwner() {
     require(msg.sender == venueOwner);
     _;
   }
 
-  modifier notOwner(){
-    require(msg.sender != venueOwner)
+  // For those who are not owner of the venue
+  modifier notOwner() {
+    require(msg.sender != venueOwner);
     _;
   }
 
-  modifier releaseTrue(){
-    require(releseEther);
+  //Changing the state
+  modifier releaseTrue() {
+    require(releaseEther);
     _;
   }
 
-  constructor (uint price, string title) public{
+  //Constructor for creating the ticket with a venue name
+  constructor (uint price, string title) public {
     ticketPrice = price;
     name = title;
     venueOwner = msg.sender;
     releaseEther = false;
   }
 
-  function unlockEther (bytes32 hash) releaseTrue public notOwner{
-    uint amount = pendingTransaction[msg.sender];
-    pendingTransaction[msg.sender] = 0;
+  //Unlocking Ether to purchase ticket for a specific address(Owner)
+  function unlockEther(bytes32 hash) releaseTrue public notOwner {
+    uint amount = pendingTransactions[msg.sender];
+    pendingTransactions[msg.sender] = 0;
     venueOwner.transfer(amount);
     tickets[hash].paidFor = true;
-    emit paidFor(true);
-  }
+    emit PaidFor(true);
+}
 
   function () public {
     releaseEther = false;
   }
 
-  function allowPurchase() public onlyOwner{
-    releseEther = True;
-    emit canPurchase(releseEther);
+  //Allowing purchase for that address
+  function allowPurchase() public onlyOwner {
+    releaseEther = true;
+    emit CanPurchase(releaseEther);
   }
 
-  function createTicket() public payable onlyOwner{
+  // Creating tickets for the venue or event
+  function createTicket() public payable onlyOwner {
     if (msg.value == ticketPrice) {
       pendingTransactions[msg.sender] = msg.value;
       bytes32 hash = keccak256(abi.encodePacked(msg.sender));
@@ -63,23 +74,28 @@ contract Tickets{
     }
   }
 
-  function getTransaction() constant public returns (uint){
-    return pendingTransaction[msg.sender];
+  // Will display if any transactions are pending for a specific address
+  function getTransaction() constant public returns (uint) {
+    return pendingTransactions[msg.sender];
   }
 
-  function getOwner(bytes32 hash) constant public returns (address){
+ // Retrieve owner address
+  function getOwner(bytes32 hash) constant public returns (address) {
     return tickets[hash].owner;
   }
 
-  function getTicketPrice() constant public return (uint){
+  // Retreive Ticket Price
+  function getTicketPrice() constant public returns (uint) {
     return ticketPrice;
   }
 
-  function getOwnerAdress() onlyOwner constant public returns (address){
+  // Get owner address
+  function getOwnerAddress() onlyOwner constant public returns (address) {
     return venueOwner;
   }
 
-  function getEventName() constant public returns (string){
+  // Get event name
+  function getEventName() constant public returns (string) {
     return name;
   }
 
